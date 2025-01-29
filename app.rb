94a456bf7fce5d49f9366a365e7893af914fc81e
kateleext <83829378+kateleext@users.noreply.github.com>
2025-01-29 05:36:11 +0000

    drafting

--- app.rb
+++ app.rb
@@ -2,8 +2,28 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:square_new)
+end
+
+get("square/new") do
+  number = params.fetch("number").to_fs
+  square = number ** 2
+  @results = {"Number" => number, "Square" => square}
+  erb(:results)
+end
+
+get("square_root/new") do
+  number = params.fetch("number").to_fs
+  square_root = number ** 0.5
+  @results = {"Number" => number, "Square Root" => square_root}
+  erb(:results)
+end
+
+get("payment/new") do
+  @apr = params.fetch("apr").to_fs(:percentage,{:precision => 4})
+  @years = params.fetch("years").to_fs
+end
+
+get(":operation/results") do
+  
 end
